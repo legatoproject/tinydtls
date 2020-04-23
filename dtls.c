88394be557307138e2e12277b60709e63ead1316
Frederic DUR <fdur@sierrawireless.com>
2020-04-23 13:34:31 +0200

    Add one removed line by DTLS resume development and fix a memory leak
    Resolves: LE-14672
    Relates: LE-6936 LE-15017
    Signed-off-by: Frederic DUR <fdur@sierrawireless.com>
    Change-Id: Id658980861af6afc6e1c485da981b8a6f5f8e739

--- dtls.c
+++ dtls.c
@@ -3444,7 +3444,9 @@ handle_handshake_msg(dtls_context_t *ctx, dtls_peer_t *peer, session_t *session,
       }
     }
 
+    dtls_handshake_free(peer->handshake_params);
     dtls_security_params_free_other(peer);
+    peer->handshake_params = NULL;
 
     dtls_debug("Handshake complete\n");
     check_stack();
